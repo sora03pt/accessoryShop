109348047cf73c1b98f6249663b4df4e8d283e9b
sora03pt <sora03pt@yahoo.co.jp>
2026-01-27 22:01:42 +0900

    no message

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,9 +8,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/favicon.svg" />
+        <link rel="icon" type="image/svg+xml" href="/accessoryShop/favicon.svg" />
+        <link rel="icon" href="/accessoryShop/favicon.ico" />
+        <link rel="apple-touch-icon" href="/accessoryShop/favicon.svg" />
         <meta name="theme-color" content="#C4BDB6" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="Jewelry Collection" />
